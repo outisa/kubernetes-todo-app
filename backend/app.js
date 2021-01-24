1f06179e7781b35195d7977783ee6a405716c8b1
outisa <savolainen.outi@gmail.com>
2021-01-24 13:47:29 +0200

    Modify structure

--- backend/app.js
+++ backend/app.js
@@ -4,17 +4,8 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('build'))
 
 const todoRouter = require('./todoRouter')
 app.use('/api/todos', todoRouter)
-
-app.get('*', (req, res) => {
-  res.sendFile(`${__dirname}/build/index.html`, (err) => {
-      if (err) {
-          res.status(500).send(err)
-      }
-  })
-})
 
 module.exports = app
