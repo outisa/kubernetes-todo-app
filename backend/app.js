b8b03b6d02ea32cc08b17637ad662d3b79d20a92
outisa <43496587+outisa@users.noreply.github.com>
2021-02-04 02:05:50 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ app.use(express.json())
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]')) 
+
 const todoRouter = require('./todoRouter')
 app.use('/api/todos', todoRouter)
 
